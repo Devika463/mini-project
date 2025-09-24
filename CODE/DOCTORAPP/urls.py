d9f257a568e137861fc90854c898175e1aa13831
Devika463 <devikasunil223@gmail.com>
2025-09-24 14:21:23 +0530

    Update urls.py

--- CODE/DOCTORAPP/urls.py
+++ CODE/DOCTORAPP/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from booking import views
 
 urlpatterns = [
+    path("", views.welcome, name="welcome"),   # Default welcome page
     path("admin/", admin.site.urls),
-    path("", include("booking.urls")),   # include booking app urls
+    path("", include("booking.urls")),
 ]
